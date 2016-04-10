85b04ceaf091da80c24236be6475c7525258fbe2
claaaaassic <johnny84131@gmail.com>
2016-04-10 17:13:09 +0800

    add file setup.py
    it can install module requests
    
    in directery ./setup.py use cmd type python setup.py install

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+	name = "requests",
+	version = "2.7.0",
+	description = "requests python module",
+	license = "Apache2 licensed",
+	author='Kenneth Reitz',
+    author_email='me@kennethreitz.com',
+	url='http://python-requests.org',
+	packages = find_packages()
+	)
